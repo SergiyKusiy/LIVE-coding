194193ca31e9c6ed60dfa97e0c57a6398967638e
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2021-11-17 11:07:21 +0200

    js

---
+++ JS/lesson2/main.js
@@ -0,0 +1,20 @@
+// How to work on tech tasks. Step by step guide
+
+// 1. Learn requirement
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Write draft solution & testing
+// 4. Refactoring & final testing -> final solution
+
+//algo
+// 1.iterate numbers from 2 to num => i
+// 2.iterate from 2 to i => number
+// 3.if i % number === 0 - not prime
+
+//2 => true
+// 3 (3 / 2, 3 / 3) => true
+// 4 (4 / 2, 4 / 3, 4 / 4) => false
+//input : num
+//output : undefine
+function getPrimes(num) {
+
+}
